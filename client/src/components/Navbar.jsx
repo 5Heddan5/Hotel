cb8dc043e473abae28f4d7fff9b0efc5f5161aee
Milad-Shirvan <miladmagic3@gmail.com>
2025-05-29 10:34:22 +0200

    Navbar
    Skapade navbaren och gjorde stylingen

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Navbar.css'; // Ensure you have this CSS file for styling
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faX } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
   return (
-    <div>Navbar</div>
-  )
+    <nav className="navbar">
+      <div className="navbar-logo">Oracel Hotel</div>
+
+      <div className="navbar-toggle" onClick={toggleMenu}>
+        <FontAwesomeIcon
+          icon={menuOpen ? faX : faBars}
+          size="lg"
+          className={`menu-icon ${menuOpen ? 'rotate' : ''}`}
+        />
+      </div>
+
+      <ul className={`navbar-links ${menuOpen ? 'active' : ''}`}>
+        <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
+        <li><Link to="/mytrips" onClick={toggleMenu}>My Trips</Link></li>
+        <li><Link to="/about" onClick={toggleMenu}>About</Link></li>
+        <li><Link to="/services" onClick={toggleMenu}>Services</Link></li>
+        <li><Link to="/profile" onClick={toggleMenu}>Profile</Link></li>
+        <li><Link to="/contact" onClick={toggleMenu}>Contact</Link></li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
